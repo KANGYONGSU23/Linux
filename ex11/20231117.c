6568e35a9f4b608d12bfab807d805ab8aefe05e6
KANGYONGSU23 <edc20203@gmail.com>
2023-11-24 16:13:02 +0900

    20231124

--- ex11/20231117.c
+++ ex11/20231117.c
@@ -37,25 +37,34 @@ int main()
 	if((pid1 = fork()) == -1)
 		onerror("fail to call fork() #1\n");
 	if(pid1 > 0)
+	{
 		if((pid2 = fork()) == -1)
 			onerror("fail to call fork() #2\n");
+		if(pid2 > 0)
+			if((pid3 = fork()) == -1)
+				onerror("fail to call fork() #3\n");
+	}	
 
 	if(pid1 > 0 && pid2 > 0 && pid3 > 0) {
 		printf("parent: %d\n", getpid());
-		close(p1[1]); close(p1[1]);
+		close(p1[1]); close(p2[1]); close(p3[1]);
 
 		FD_ZERO(&initset);
 		FD_SET(p1[0], &initset);
 		FD_SET(p2[0], &initset);
+		FD_SET(p3[0], &initset);
 
 		newset = initset;
-		while(select(p2[0] + 1, &newset, NULL, NULL, NULL) > 0) {
+		while(select(p3[0] + 1, &newset, NULL, NULL, NULL) > 0) {
 			if(FD_ISSET(p1[0], &newset))
 				if(read(p1[0], msg, MSGSIZE) > 0)
 					printf("[parent] %s from child1\n", msg);
 			if(FD_ISSET(p2[0], &newset))
 				if(read(p2[0], msg, MSGSIZE) > 0)
 					printf("[parent] %s from child2\n", msg);
+			if(FD_ISSET(p3[0], &newset))
+				if(read(p3[0], msg, MSGSIZE) > 0)
+					printf("[parent] %s from child3\n", msg);
 			newset = initset;
 		}
 	}
@@ -71,7 +80,7 @@ int main()
 		printf("child1: bye!\n");
 		exit(0);
 	}
-	else if(pid1 > 0 && pid2 == 0 && pid3 > 0) {
+	else if(pid1 > 0 && pid2 == 0 && pid3 == 0) {
 		printf("child2: %d\n", getpid());
 		close(p1[0]); close(p1[1]); close(p2[0]); close(p3[0]); close(p3[1]);
 
@@ -92,7 +101,7 @@ int main()
 			printf("child3: send message %d\n", i);
 			write(p3[1], "i'm child3", MSGSIZE);
 		}
-		printf("child3: byb!\n");
+		printf("child3: bye!\n");
 		exit(0);
 	}
 }
